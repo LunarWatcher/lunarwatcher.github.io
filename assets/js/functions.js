467e49a7880714e0fde7b35ac8302f0a93496562
Olivia Zoe <zoe.i2k1@gmail.com>
2019-12-22 13:19:12 +0100

    Hugo attempt 2 [Previous attempts erased]

--- assets/js/functions.js
+++
@@ -1,9 +0,0 @@
-
-function openSidenav() {
-    document.getElementById("sidenav").style.width = "100%";
-}
-
-function closeSidenav() {
-    document.getElementById("sidenav").style.width = "0%";
-}
-
