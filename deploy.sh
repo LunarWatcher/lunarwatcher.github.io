b84ab3283009e8eece775732f321217cf996c08e
Olivia Zoe <zoe.i2k1@gmail.com>
2019-12-22 13:28:56 +0100

    Update deployment system to use branching

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build
+hugo 
+
+cd public
+
+# Commit
+git add .
+
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
