b661d9bd683aee590118458a0108380fac10dbf8
Olivia <zoe.i2k1@gmail.com>
2021-08-16 00:30:05 +0200

    This has to be a warcrime

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,6 +7,47 @@ function closeSidenav() {
     document.getElementById("sidenav").style.width = "0%";
 }
 
+function rand(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function uwu() {
+    document.getElementById("uwu").text = "OWO!!";
+    let emojis = [
+        "owo", "OwO",
+        "uwu", "UwU",
+        ":3", "x3"
+    ];
+    let otherCrap = [
+        "rawr x3",
+        "<i>notices button</i>"
+    ];
+
+    let body = document.body.getElementsByTagName("main")[0];
+    let queue = [...body.children];
+    while (queue.length > 0) {
+        let element = queue.shift();
+        if (element.children.length > 0) {
+            queue.push(...element.children);
+        }
+        [...element.childNodes].filter((node) => {
+            return (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== "");
+        }).forEach((node) => {
+            node.textContent = node.textContent.replace(/[lr]/ig, (str) => { 
+                return str[0].toLowerCase() == str[0] ? "w" : "W";
+            }).replace(/[.!?]/gi, (str) => {
+                let char = str[0];
+                if (Math.random() > 0.7) {
+                    return char + " " + rand(emojis);
+                }
+                return char;
+            });
+        });
+    }
+    console.log(body);
+
+}
+
 window.onload=function() {
     let greetings = [
         "Hiya. Whatcha doing there? :eyes:",
@@ -16,6 +57,5 @@ window.onload=function() {
         "Self-destruct initiated",
         "Don't h4xx0r me @ 127.0.0.1, thanks in advance"
     ];
-    const random = Math.floor(Math.random() * greetings.length);
-    console.log(random, greetings[random]);
+    console.log(rand(greetings));
 };
