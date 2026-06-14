59c3f86be1c2148edecb1eef7f30380528946092
Olivia <oliviawolfie@pm.me>
2026-06-14 02:42:57 +0200

    Add a clock (because why not?)

--- assets/js/index-fun.js
+++ assets/js/index-fun.js
@@ -4,3 +4,23 @@ document.getElementById("profile-pic")
             document.getElementById("profile-pic-popover").togglePopover();
         }
     });
+
+const dateFormatter = new Intl.DateTimeFormat([], {
+    timeZone: "Europe/Oslo",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "longOffset"
+});
+
+setTimeout(() => {
+    setInterval(() => {
+        document.getElementById("current-time").innerText =
+            dateFormatter.format(new Date());
+    }, 1000);
+},
+    // Align the interval with each second actually flipping
+    // (My OS taskbar has the time with seconds, so any drift greatly annoys me)
+    1000 - new Date().getMilliseconds()
+);
